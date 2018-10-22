a07561712292c23d59e790082941199ceb0ac03f
rishiraj-ldpl <rishiraj@mylinchpin.tech>
2018-10-22 12:22:21 +0530

    navigate the pages+

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 		  <a class="nav-link" href="#">Unit</a>
 		</li>
 		<li class="nav-item">
-		  <a class="nav-link" href="#">Log Out</a>
+		  <a class="nav-link" href="login.php">Log Out</a>
 		</li>		 
 	  </ul>
 	  </div>  
