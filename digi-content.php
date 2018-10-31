1c9cc173d307f9e5a0eaa909b01ec79e25fb0f63
shubham <shubham@mylinchpin.tech>
2018-10-31 16:56:39 +0530

    digi-content

--- digi-content.php
+++ digi-content.php
@@ -78,12 +78,10 @@
     <!-- FOOTER START -->
     <?php include 'footer.php' ?>
     <!-- FOOTER END -->
-
     <!-- Custom scripts for this template -->
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/site.js">
     </script>
 </body>
-
 </html>
