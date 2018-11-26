42716151caab5e21bd4d5985a1a94301652ecc75
shubham <shubham@mylinchpin.tech>
2018-11-26 19:00:36 +0530

    student-deshboard

---
+++ student-dashboard.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Edugen</title>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- HEADER START -->
+    <?php include 'header.php' ?>
+    <!-- HEADER END -->
+    <!-- CONTENT START -->
+    <div class="content pdt dashboard-bg">
+        <div class="breadcrumb-wrap">
+            <ul class="breadcrumb container">
+                <li><a href="teacher-dashboard.php">Dashboard</a></li>
+            </ul>
+        </div>
+        <div class="summary-page" id="dashboard">
+            <div class="wrapper-card pt-5 pb-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 my-auto">
+                            <div class="card card-stats">
+                                <div class="card-header card-header-danger card-header-icon">
+                                    <div class="card-icon">
+                                        Class - 2
+                                    </div>
+                                    <h2 class="card-title">Science</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-9 mx-auto">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-3 col-sm-6 card-margin">
+                                    <figure class="card-overlay">
+                                        <img class="img-fluid" src="assets/img/front-page.jpg" alt="front-page.jpg">
+                                        <a href="content.php" title="Go to Content"><span>Content</span>
+                                            <span class="click-here"> Click here</span>
+                                        </a>
+                                        <a href="question.php" title="Go to Worksheet"><span>Question</span>
+                                            <span class="click-here"> Click here</span>
+                                        </a>
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- FOOTER START -->
+    <?php include 'footer.php' ?>
+    <!-- FOOTER END -->
+    <!-- Custom scripts for this template -->
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/site.js"></script>
+</body>
+
+</html>
