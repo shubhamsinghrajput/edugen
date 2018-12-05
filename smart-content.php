1f48ba5ddb628fa31264a96226ee09d6fb7194b0
rishiraj-ldpl <rishiraj@mylinchpin.tech>
2018-12-05 16:20:35 +0530

    samrt contet background animation

--- smart-content.php
+++ smart-content.php
@@ -12,17 +12,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css">
+    <link href="assets/css/bg-animate.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
+     
 </head>
 
 <body class="content-page">
+   <canvas id='c' style="z-index:-1;"></canvas>
+   
     <!-- HEADER START -->
     <?php include 'student-header.php' ?>
     <!-- HEADER END -->
 
     <!-- CONTENT START -->
     <div class="content pdt pb-0">
-        <div class="breadcrumb-wrap">
+      <div class="breadcrumb-wrap">
             <div class="container d-sm-flex justify-content-sm-between">
                 <ul class="breadcrumb">
                     <li><a href="student-dashboard.php">Dashboard</a></li>
@@ -34,19 +38,12 @@
                 <a href="assets/video/class-one/index.html" class="digi-link mr-2 ml-sm-4 mt-2 mt-sm-0">Digi Content</a>
             </div>
         </div>
-        <div class="container p-0">
-<!--            <p data-height="265" data-theme-id="0" data-slug-hash="Gtyrw" data-default-tab="js,result" data-user="olgundutkan" data-pen-title="jquery-paint" class="codepen">See the Pen <a href="https://codepen.io/olgundutkan/pen/Gtyrw/">jquery-paint</a> by olgundutkan (<a href="https://codepen.io/olgundutkan">@olgundutkan</a>) on <a href="https://codepen.io">CodePen</a>.</p>-->
-            
-            
-<!--
-            <iframe height='265' scrolling='no' title='jquery-paint' src='//codepen.io/olgundutkan/embed/Gtyrw/?height=265&theme-id=0&default-tab=js,result' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'>See the Pen <a href='https://codepen.io/olgundutkan/pen/Gtyrw/'>jquery-paint</a> by olgundutkan (<a href='https://codepen.io/olgundutkan'>@olgundutkan</a>) on <a href='https://codepen.io'>CodePen</a>.
-            </iframe>
--->
-            
-<!--            <script async src="https://static.codepen.io/assets/embed/ei.js"></script>-->
-<!--   <embed src="smart-content/samples/book/index.html" type="">-->
-        <iframe src="smart-content5/samples/book/index.html" frameborder="0" width="100%" height="700px"></iframe>
-         </div>
+        <div class="container p-0 ">
+                      <iframe style="position:relative; z-index:2;" src="smart-content/samples/book/index.html" frameborder="0" width="100%" height="700px"></iframe>
+        </div>
+        
+        
+       
     </div>
     <!-- FOOTER START -->
     <?php include 'footer.php' ?>
@@ -54,8 +51,10 @@
     <!-- Custom scripts for this template -->
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/site.js">
-    </script>
-</body>
+    <script src='https://rawgit.com/akm2/simplex-noise.js/master/simplex-noise.js'></script>
+    <script src='assets/js/kzymdn.js'></script>
+    <script src="assets/js/index.js"></script>
+    <script src="assets/js/site.js"></script>
+ </body>
 
 </html>
