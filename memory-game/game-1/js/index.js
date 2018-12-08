4847a197182198ddcae74415322903d603f42fba
rishiraj-ldpl <rishiraj@mylinchpin.tech>
2018-12-08 15:49:35 +0530

    game fixed

--- memory-game/game-1/js/index.js
+++ memory-game/game-1/js/index.js
@@ -12,75 +12,80 @@ var sock1Id = '';
 var sock2Id = '';
 
 //put unique IDs on each card on the board
-$('.flip').each(function(i) {
-	$(this).attr('id', i + 1);
+$('.flip').each(function (i) {
+    $(this).attr('id', i + 1);
 });
 
-$('.back img').each(function(i) {
-	var theSock = '' + socks.splice(getRandomInt(0, socks.length - 1), 1);
-	var theCard = $(this).parent().parent();
-	console.log(theCard);
-	theCard.addClass(theSock);
-	$(this).attr('src', url + theSock + 'sock.png')
+$('.back img').each(function (i) {
+    var theSock = '' + socks.splice(getRandomInt(0, socks.length - 1), 1);
+    var theCard = $(this).parent().parent();
+    console.log(theCard);
+    theCard.addClass(theSock);
+    $(this).attr('src', url + theSock + 'sock.png')
 });
 
 function getRandomInt(min, max) {
-	return Math.ceil(Math.random() * (max - min) + min);
+    return Math.ceil(Math.random() * (max - min) + min);
 }
 
-$('.flip').click(function(e) {
-	// $(this).off();
-	var theCard = $(this).find('.card');
-	
-	//allow only 2 flipped cards?
-	if ($('.flipped').length < 2) {
-		theCard.addClass('flipped');
-		var classList = theCard.attr('class');
+$('.flip').click(function (e) {
+    // $(this).off();
+    var theCard = $(this).find('.card');
 
-		classList = classList.split(' ');
+    //allow only 2 flipped cards?
+    if ($('.flipped').length < 2) {
+        theCard.addClass('flipped');
+        var classList = theCard.attr('class');
 
-		var theColor = classList[1];
-		console.log(theColor);
-		if (sock1 == '') {
-			sock1 = theColor;
-			sock1Id = theCard.parent().attr('id');
-		} else {
-			sock2 = theColor;
-			sock2Id = theCard.parent().attr('id');
-			checkMatch();
-		}
+        classList = classList.split(' ');
 
-	}
+        var theColor = classList[1];
+        console.log(theColor);
+        if (sock1 == '') {
+            sock1 = theColor;
+            sock1Id = theCard.parent().attr('id');
+        } else {
+            sock2 = theColor;
+            sock2Id = theCard.parent().attr('id');
+            checkMatch();
+        }
+
+    }
 
 });
-function checkMatch(){
-		if (sock1 == sock2 && sock2Id != sock1Id ) {
-			console.log('match');
-			$('.' + sock2 + '.flipped').parent().off().addClass('matched');
-			$('.' + sock2 + '.flipped .front img').attr('src', url + 'match.png');
-			$('.' + sock2 + '.flipped .back').removeClass('back face');
-			$('.' + sock2 + '.flipped .front').css({'background-color':'transparent'});
-			//call flipback and checkwin here
-			setTimeout(flipBack, 800);
-			checkWin();
-		} else {
-			//call flipback
-			setTimeout(flipBack, 800);
-		}
+
+function checkMatch() {
+    if (sock1 == sock2 && sock2Id != sock1Id) {
+        console.log('match');
+        $('.' + sock2 + '.flipped').parent().off().addClass('matched');
+        $('.' + sock2 + '.flipped .front img').attr('src', url + 'match.png');
+        $('.' + sock2 + '.flipped .back').removeClass('back face');
+        $('.' + sock2 + '.flipped .front').css({
+            'background-color': 'transparent'
+        });
+        //call flipback and checkwin here
+        setTimeout(flipBack, 800);
+        checkWin();
+    } else {
+        //call flipback
+        setTimeout(flipBack, 800);
+    }
 }
 
-function flipBack(){
-	$('.flipped').removeClass('flipped');
-	sock1 = '';
-	sock2 = '';
-	sock1Id = '';
-	sock2Id = '';
+function flipBack() {
+    $('.flipped').removeClass('flipped');
+    sock1 = '';
+    sock2 = '';
+    sock1Id = '';
+    sock2Id = '';
 }
 
-function checkWin(){
-	if( $('.matched').length == $('.flip').length ){
-		//all the cards are matched
-		message = '<h2>You have matched all the socks!</h2>';
-		$('#stage').prepend('div').html(message);
-		}
+function checkWin() {
+    if ($('.matched').length == $('.flip').length) {
+        //all the cards are matched
+        message = '<h2>You have matched all the socks!</h2>' + "<br>" + '<input type="reset" onclick=" location.reload()">';
+         
+        $('#stage').prepend('div').html(message);
+
+    }
 }
